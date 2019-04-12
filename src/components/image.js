65fd24ecc083a3ab80a1b9af2b2f347265b27a1c
DisjunkteMenge <49205335+DisjunkteMenge@users.noreply.github.com>
2019-04-12 15:19:03 +0200

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -19,7 +19,7 @@ const Image = () => (
       query {
         placeholderImage: file(relativePath: { eq: "a.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 100) {
               ...GatsbyImageSharpFluid
             }
           }
